429936bb2df9e4132813408245883fb95577720c
Kazuki Imamura <imamura@info-lounge.jp>
2023-01-13 10:03:13 +0900

    HeaderにemotionとLinkの記述例を追加

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,31 @@
+import { css } from '@emotion/react'
+import Link from 'next/link'
+
+const navigationStyle = css`
+  display: flex;
+  list-style: none;
+  li {
+    margin-right: 12px;
+  }
+`
+
 const Header = () => {
-  return <header>header</header>
+  return (
+    <>
+      <header>header</header>
+      <ul css={navigationStyle}>
+        <li>
+          <Link href="/">ホーム</Link>
+        </li>
+        <li>
+          <Link href="/data">データ一覧</Link>
+        </li>
+        <li>
+          <Link href="/group">研究グループ一覧</Link>
+        </li>
+      </ul>
+    </>
+  )
 }
 
 export default Header
