9b0d37951863789dec62cb49b9e8ad25cf43f7b5
Kazuki Imamura <imamura@info-lounge.jp>
2023-01-13 10:02:29 +0900

    ページを追加

---
+++ src/pages/group/index.tsx
@@ -0,0 +1,9 @@
+import DefaultLayout from '@/components/Layout/DefaultLayout'
+
+const GroupIndex = () => {
+  return (
+    <DefaultLayout title="研究グループ一覧">研究グループ一覧</DefaultLayout>
+  )
+}
+
+export default GroupIndex
