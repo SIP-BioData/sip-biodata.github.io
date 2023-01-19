62199fdb6af527b81cc856e4cc44125e694a2dec
ikari <ikari@info-lounge.jp>
2023-01-19 16:15:14 +0900

    Add icon and Adjust style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,9 +13,10 @@ const footerLogoStyle = css`
   max-width: var(--inner1000);
   margin: 0 auto;
   display: flex;
-  align-items: center;
+  align-items: flex-end;
   h1 {
-    font-size: 
+    font-size: 20px;
+    line-heoght: 20px;
     margin-left: 22px;
   }
 `
@@ -26,6 +27,16 @@ const menuStyle = css`
   display: flex;
   li {
     margin-right: 36px
+  }
+`
+
+const linkStyle = css`
+  display: flex;
+  align-items: center;
+  svg {
+    width: 14px;
+    height: 14px;
+    margin-left: 8px;
   }
 `
 
@@ -42,8 +53,13 @@ const Footer = () => {
             <Link href="/data">データリスト</Link>
           </li>
           <li>
-            <a href="#" target="_blank" rel="noopener noreferrer">
-              SIP BioDB Search
+            <a css={linkStyle} href="#" target="_blank" rel="noopener noreferrer">
+              <span>SIP BioDB Search</span>
+              <svg css={css`fill: #fff;`} xmlns="http://www.w3.org/2000/svg"
+                   viewBox="0 0 16 16">
+                <path className="st0" d="M9.8,0v1.8H13l-8.7,8.7l1.3,1.3L14.2,3v3.2H16V0 M14.2,14.2H1.8V1.8H8V0H1.8C0.8,0,0,0.8,0,1.8c0,0,0,0,0,0
+	v12.4c0,1,0.8,1.8,1.8,1.8h12.4c1,0,1.8-0.8,1.8-1.8V8h-1.8V14.2z"/>
+              </svg>
             </a>
           </li>
         </ul>
