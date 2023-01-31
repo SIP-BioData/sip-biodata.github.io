d3cb4c47bdc9df0a1a6c4a6b3f5d822a6db4bebd
ikari <ikari@info-lounge.jp>
2023-01-31 16:47:10 +0900

    Delete extra tags

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import {css} from "@emotion/react";
+import { css } from '@emotion/react'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -26,7 +26,7 @@ const menuStyle = css`
   margin: 56px auto 0;
   display: flex;
   li {
-    margin-right: 36px
+    margin-right: 36px;
   }
 `
 
@@ -42,29 +42,35 @@ const linkStyle = css`
 
 const Footer = () => {
   return (
-    <>
-      <footer css={footerStyle}>
-        <div css={footerLogoStyle}>
-          <Image src={logoPic} alt="SIP"/>
-          <h1>SIP「スマートバイオ産業・農業基盤技術」データ連携ポータル</h1>
-        </div>
-        <ul css={menuStyle}>
-          <li>
-            <Link href="/data">データリスト</Link>
-          </li>
-          <li>
-            <a css={linkStyle} href="#" target="_blank" rel="noopener noreferrer">
-              <span>SIP BioDB Search</span>
-              <svg css={css`fill: #fff;`} xmlns="http://www.w3.org/2000/svg"
-                   viewBox="0 0 16 16">
-                <path className="st0" d="M9.8,0v1.8H13l-8.7,8.7l1.3,1.3L14.2,3v3.2H16V0 M14.2,14.2H1.8V1.8H8V0H1.8C0.8,0,0,0.8,0,1.8c0,0,0,0,0,0
-	v12.4c0,1,0.8,1.8,1.8,1.8h12.4c1,0,1.8-0.8,1.8-1.8V8h-1.8V14.2z"/>
-              </svg>
-            </a>
-          </li>
-        </ul>
-      </footer>
-    </>
+    <footer css={footerStyle}>
+      <div css={footerLogoStyle}>
+        <Image src={logoPic} alt="SIP" />
+        <h1>SIP「スマートバイオ産業・農業基盤技術」データ連携ポータル</h1>
+      </div>
+      <ul css={menuStyle}>
+        <li>
+          <Link href="/data">データリスト</Link>
+        </li>
+        <li>
+          <a css={linkStyle} href="#" target="_blank" rel="noopener noreferrer">
+            <span>SIP BioDB Search</span>
+            <svg
+              css={css`
+                fill: #fff;
+              `}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+            >
+              <path
+                className="st0"
+                d="M9.8,0v1.8H13l-8.7,8.7l1.3,1.3L14.2,3v3.2H16V0 M14.2,14.2H1.8V1.8H8V0H1.8C0.8,0,0,0.8,0,1.8c0,0,0,0,0,0
+v12.4c0,1,0.8,1.8,1.8,1.8h12.4c1,0,1.8-0.8,1.8-1.8V8h-1.8V14.2z"
+              />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </footer>
   )
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,7 +24,7 @@ const navigationStyle = css`
   background-color: var(--col-wh);
   padding: 22px 36px;
   border-radius: 36px;
-  line-heignt: 70px
+  line-heignt: 70px;
   h1 {
     font-size: 18px;
   }
@@ -49,35 +49,40 @@ const linkStyle = css`
 
 const Header = () => {
   return (
-    <>
-      <header css={headerStyle}>
-        <div css={logoStyle}>
-          <Image src={logoImg} alt="SIP" />
-        </div>
-        <nav css={navigationStyle}>
-          <h1>
-            <Link href="/">
-              SIP「スマートバイオ産業・農業基盤技術」データ連携ポータル
-            </Link>
-          </h1>
-          <ul css={menuStyle}>
-            <li>
-              <Link href="/data">データリスト</Link>
-            </li>
-            <li>
-              <a css={linkStyle} href="#" target="_blank" rel="noopener noreferrer">
-                <span>SIP BioDB Search </span>
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 16 16">
-                  <path className="st0" d="M9.8,0v1.8H13l-8.7,8.7l1.3,1.3L14.2,3v3.2H16V0 M14.2,14.2H1.8V1.8H8V0H1.8C0.8,0,0,0.8,0,1.8c0,0,0,0,0,0
-	v12.4c0,1,0.8,1.8,1.8,1.8h12.4c1,0,1.8-0.8,1.8-1.8V8h-1.8V14.2z"/>
-                </svg>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header css={headerStyle}>
+      <div css={logoStyle}>
+        <Image src={logoImg} alt="SIP" />
+      </div>
+      <nav css={navigationStyle}>
+        <h1>
+          <Link href="/">
+            SIP「スマートバイオ産業・農業基盤技術」データ連携ポータル
+          </Link>
+        </h1>
+        <ul css={menuStyle}>
+          <li>
+            <Link href="/data">データリスト</Link>
+          </li>
+          <li>
+            <a
+              css={linkStyle}
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>SIP BioDB Search </span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                <path
+                  className="st0"
+                  d="M9.8,0v1.8H13l-8.7,8.7l1.3,1.3L14.2,3v3.2H16V0 M14.2,14.2H1.8V1.8H8V0H1.8C0.8,0,0,0.8,0,1.8c0,0,0,0,0,0
+v12.4c0,1,0.8,1.8,1.8,1.8h12.4c1,0,1.8-0.8,1.8-1.8V8h-1.8V14.2z"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
