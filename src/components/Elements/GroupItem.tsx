799972f7338d8893abc7d7d7daff94925304d2ea
ikari <ikari@info-lounge.jp>
2023-01-31 16:42:46 +0900

    Add state hook, etc

--- src/components/Elements/GroupItem.tsx
+++ src/components/Elements/GroupItem.tsx
@@ -1,30 +1,30 @@
 import { css } from '@emotion/react'
 import Link from 'next/link'
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 
 type Props = {
   group: string
   data: string
   target: string
   provider: string
-  accesslevel: string
+  accessLevel: string
   content: string
   storage: string
-  dataform: string
+  dataForm: string
   admin: string
   publicPrivate: string
   database: ReactNode
-  reference?: string
+  reference: string
 }
 
 const style = css`
   border: 1px solid #707070;
   padding: 28px;
-  
+
   dl {
-    display: flex;  
+    display: flex;
     padding: 12px 0 14px;
-    
+
     dt {
       width: 120px;
       margin-right: 30px;
@@ -34,44 +34,50 @@ const style = css`
 `
 
 const borderStyle = css`
-  border-bottom: 1px solid #EAEAEA;
+  border-top: 1px solid #eaeaea;
 `
 
 const linkStyle = css`
-  color: #0068D0
+  color: #0068d0;
 `
 
 const layoutStyle = css`
   margin-top: 20px;
 `
 
-
-
 const GroupItem = ({
-   group,
-   data,
-   target,
-   provider,
-   accesslevel,
-   content,
-   storage,
-   dataform,
-   admin,
-   publicPrivate,
-   database,
-   reference
- }: Props) => {
+  group,
+  data,
+  target,
+  provider,
+  accessLevel,
+  content,
+  storage,
+  dataForm,
+  admin,
+  publicPrivate,
+  database,
+  reference,
+}: Props) => {
+  const [isDisplayAll, setIsDisplayAll] = useState(false)
+
   return (
     <section css={style}>
       <dl>
         <dt>研究グループ</dt>
         <dd>
-          <Link css={linkStyle} href="">{group}</Link>
+          <Link css={linkStyle} href="">
+            {group}
+          </Link>
         </dd>
       </dl>
       <dl>
         <dt>データ</dt>
-        <dd><Link css={linkStyle} href="">{data}</Link></dd>
+        <dd>
+          <Link css={linkStyle} href="">
+            {data}
+          </Link>
+        </dd>
       </dl>
       <dl>
         <dt>対象</dt>
@@ -81,41 +87,48 @@ const GroupItem = ({
         <dt>提供者</dt>
         <dd>{provider}</dd>
       </dl>
-      <dl css={borderStyle}>
-        <dt>アクセスレベル</dt>
-        <dd>{accesslevel}</dd>
-      </dl>
-      <dl css={borderStyle}>
-        <dt>格納されるデータの内容</dt>
-        <dd>{content}</dd>
-      </dl>
-      <dl css={borderStyle}>
-        <dt>データベース・データ格納場所</dt>
-        <dd>{storage}</dd>
-      </dl>
-      <dl css={borderStyle}>
-        <dt>データ形式</dt>
-        <dd>{dataform}</dd>
-      </dl>
-      <dl css={borderStyle}>
-        <dt>データ管理者</dt>
-        <dd>{admin}</dd>
-      </dl>
-      <dl css={borderStyle}>
-        <dt>公開（公開日）・非公開</dt>
-        <dd>{publicPrivate}</dd>
-      </dl>
-      <dl css={borderStyle}>
-        <dt>連携可能なデータベース</dt>
-        <dd>{database}</dd>
-      </dl>
       <dl>
-        <dt>参考</dt>
-        <dd>{reference}</dd>
+        <dt>アクセスレベル</dt>
+        <dd>{accessLevel}</dd>
       </dl>
-      <div css={layoutStyle}>
-        <Link css={linkStyle} href="">表示内容を減らす</Link>
-      </div>
+      {isDisplayAll && (
+        <>
+          <dl css={borderStyle}>
+            <dt>格納されるデータの内容</dt>
+            <dd>{content}</dd>
+          </dl>
+          <dl css={borderStyle}>
+            <dt>データベース・データ格納場所</dt>
+            <dd>{storage}</dd>
+          </dl>
+          <dl css={borderStyle}>
+            <dt>データ形式</dt>
+            <dd>{dataForm}</dd>
+          </dl>
+          <dl css={borderStyle}>
+            <dt>データ管理者</dt>
+            <dd>{admin}</dd>
+          </dl>
+          <dl css={borderStyle}>
+            <dt>公開（公開日）・非公開</dt>
+            <dd>{publicPrivate}</dd>
+          </dl>
+          <dl css={borderStyle}>
+            <dt>連携可能なデータベース</dt>
+            <dd>{database}</dd>
+          </dl>
+          <dl>
+            <dt>参考</dt>
+            <dd>{reference}</dd>
+          </dl>
+          <div css={layoutStyle}>
+            <Link css={linkStyle} href="">
+              表示内容を減らす
+            </Link>
+          </div>
+        </>
+      )}
+      <button onClick={() => setIsDisplayAll(true)}>more</button>
     </section>
   )
 }
