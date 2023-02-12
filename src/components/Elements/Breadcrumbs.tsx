a068d7c0b00fd22a5e2dbe899ed5399ed55944ee
Kazuki Imamura <imamura@info-lounge.jp>
2023-02-12 23:03:20 +0900

    Breadcrumbsコンポーネントを修正

--- src/components/Elements/Breadcrumbs.tsx
+++ src/components/Elements/Breadcrumbs.tsx
@@ -1,45 +1,49 @@
 import { css } from '@emotion/react'
 import Link from 'next/link'
 
-type Props = {
-  title?: string
+type Item = {
+  label: string
   path?: string
-  childTitle?: string
+}
+
+type Props = {
+  items: Item[]
 }
 
 const style = css`
   margin: 20px 0 0;
   display: flex;
-  Link {
-    margin-left: 12px;
-    &:last-child {
-      color: #646464;
-    }
-  }
+  flex-wrap: wrap;
 `
-const childStyle = css`
+
+const itemStyle = css`
+  margin-bottom: 8px;
   &::before {
     content: '>';
     margin: 0 8px;
   }
   &:last-child {
-    color: #646464;
-  }
-  &:empty {
-    display: none;
+    color: var(--col-gr);
   }
 `
 
-const Breadcrumbs = ({ title, childTitle, path }: Props) => {
+const Breadcrumbs = (props: Props) => {
   return (
-    <nav css={style}>
-      <Link href="/">HOME</Link>
-      <Link css={childStyle} href="">
-        {title}
-      </Link>
-      <Link css={childStyle} href="">
-        {childTitle}
-      </Link>
+    <nav>
+      <ul css={style}>
+        <li>
+          <Link href="/">HOME</Link>
+        </li>
+        {props.items.map((item, index) => (
+          <li key={index} css={itemStyle}>
+            {item.path ? (
+              <Link href={item.path}>{item.label}</Link>
+            ) : (
+              item.label
+            )}
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
