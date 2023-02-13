8226c0ad09e30652cb489c7aebb188537cb9ae79
Kazuki Imamura <imamura@info-lounge.jp>
2023-02-13 19:04:29 +0900

    getDataStaticPropsを追加

--- src/pages/data/[id].tsx
+++ src/pages/data/[id].tsx
@@ -1,25 +1,21 @@
 import { css } from '@emotion/react'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
 
 import DataTable from '@/components/Elements/DataTable'
 import LinkButton from '@/components/Elements/LinkButton'
 import Layout from '@/components/Layout/Layout'
 import LowerPageLayout from '@/components/Layout/LowerPageLayout'
 import Breadcrumbs from '@/components/Navigation/Breadcrumbs'
-import { getDatabaseStaticProps, getDataStaticPaths } from '@/lib/static'
+import { getDataStaticPaths, getDataStaticProps } from '@/lib/static'
 
 import arrowBack from '../../../public/arrowBack.svg'
 
-export const getStaticProps = getDatabaseStaticProps
+export const getStaticProps = getDataStaticProps
 export const getStaticPaths = getDataStaticPaths
 
 type Props = {
   title?: string
-  sipDatabase: Item[]
-  integbioDatabase: Item[]
-  sipDatabaseColumn: Item[]
-  integbioDatabaseColumn: Item[]
+  data: Item
+  columns: Item
 }
 
 const sectionStyle = css`
@@ -38,40 +34,30 @@ const buttonContainerStyle = css`
 `
 
 const DataDetail = (props: Props) => {
-  const router = useRouter()
-  const { id } = router.query
-  const [data, setData] = useState<Item | null>(null)
-  const mergedDatabase = [...props.sipDatabase, ...props.integbioDatabase]
-  const columns = [...props.sipDatabaseColumn, ...props.integbioDatabaseColumn]
-  const columnsObject = Object.assign(columns[0], columns[1])
-
   const breadcrumbs = [
     {
       label: 'データリスト',
       path: '/data',
     },
     {
-      label: data ? `${data.name}` : 'データ詳細',
+      label: props.data ? `${props.data.name}` : 'データ詳細',
     },
   ]
 
-  useEffect(() => {
-    const currentData = mergedDatabase.find((v) => v.id === id) || null
-    setData(currentData)
-  }, [props, id])
-
   return (
-    <Layout title={data ? `${data.name}` : 'データ詳細'}>
+    <Layout title={props.data ? `${props.data.name}` : 'データ詳細'}>
       <LowerPageLayout>
         <Breadcrumbs items={breadcrumbs} />
         <section css={sectionStyle}>
-          {data && (
+          {props.data && (
             <h1>
-              <span>{data.name}</span>
-              {data.group_name && <span>{`/ ${data.group_name}`}</span>}
+              <span>{props.data.name}</span>
+              {props.data.group_name && (
+                <span>{`/ ${props.data.group_name}`}</span>
+              )}
             </h1>
           )}
-          <DataTable item={data} columns={columnsObject} />
+          <DataTable item={props.data} columns={props.columns} />
           <div css={buttonContainerStyle}>
             <LinkButton path="/data" leftIcon={arrowBack}>
               データリストに戻る
