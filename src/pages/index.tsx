153d6bbbe4e2733e1c2f9737b813484c730025fb
Kazuki Imamura <imamura@info-lounge.jp>
2023-02-13 16:10:47 +0900

    lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { css } from '@emotion/react'
 import Image from 'next/image'
 
-import LinkButton from '@/components/Elements/LinkButton'
 import HomeItem from '@/components/Elements/HomeItem'
+import LinkButton from '@/components/Elements/LinkButton'
 import Layout from '@/components/Layout/Layout'
 
 import aboutImg from '../../public/aboutImg.png'
