724eb20bbc3484d4127514312630bc658715e520
Kazuki Imamura <imamura@info-lounge.jp>
2023-02-28 16:41:57 +0900

    scriptの不要な部分を削除

--- script/sip-csv-to-json.py
+++ script/sip-csv-to-json.py
@@ -8,7 +8,6 @@ df_sip = pd.read_csv("../sip_database.csv", usecols=cols)
 
 with open("../csv/sip_database_column.csv", "r") as csv_sip_column:
     dict_sip_column = csv.DictReader(csv_sip_column)
-    df_sip = df_sip.iloc[:, 0:len(df_sip_column.columns)]
     for sip_column in dict_sip_column:
       df_sip.rename(columns=sip_column, inplace=True)
       df_sip.to_json("../data/sip_database.json", orient="records")
