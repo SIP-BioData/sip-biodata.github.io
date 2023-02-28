863d3ada4c37e9e3f87f66ce394776674cd8ae46
Kazuki Imamura <imamura@info-lounge.jp>
2023-02-28 15:38:08 +0900

    debug

--- script/sip-csv-to-json.py
+++ script/sip-csv-to-json.py
@@ -1,11 +1,13 @@
 import csv
 import pandas as pd
 
+with open("../sip_database.csv") as f:
+    print(f.read())
+
 df_sip_column = pd.read_csv("../csv/sip_database_column.csv")
 columns = df_sip_column[1:1]
 cols = [str(column) for column in columns]
 df_sip = pd.read_csv("../sip_database.csv", usecols=cols)
-print(df_sip)
 
 with open("../csv/sip_database_column.csv", "r") as csv_sip_column:
     dict_sip_column = csv.DictReader(csv_sip_column)
